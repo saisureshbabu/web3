49a4594cfc2caa996937583c96e9d91e2772c2d2
Suresh Babu <suresh@knowesis.com>
2016-04-11 11:48:14 +0800

    added utility class to get date and tostring

---
+++ Util.java
@@ -0,0 +1,14 @@
+import java.util.Date
+
+
+public class Utility{
+    	public String getDate()
+	{
+	   return new Date();
+	}
+	
+	public String stringToString()
+	{
+	   return new String();
+	}
+}
